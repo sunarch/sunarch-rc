85e517a8d15ebc7a7c90c6d2f42e1a102163cc1e
András Németh <sunarch@protonmail.com>
2023-06-23 16:57:53 +0200

    Update IRC clients

--- ubuntu/zz-setup/packages/02-comms.sh
+++ ubuntu/zz-setup/packages/02-comms.sh
@@ -22,12 +22,12 @@ sudo apt-get install thunderbird # Email, RSS and newsgroup client with integrat
 # ---------------------------------------------------------------------------- #
 # IRC:
 
-sudo apt-get install polari # Internet Relay Chat (IRC) client
-sudo apt-get install quassel # distributed IRC client - monolithic core+client
-sudo apt-get install smuxi # graphical IRC client
+#sudo apt-get install polari # Internet Relay Chat (IRC) client
+#sudo apt-get install quassel # distributed IRC client - monolithic core+client
+#sudo apt-get install smuxi # graphical IRC client
 
-sudo apt-get install hexchat # IRC client for X based on X-Chat 2
-sudo apt-get install hexchat-indicator # Hexchat Indicator Plugin
+#sudo apt-get install hexchat # IRC client for X based on X-Chat 2
+#sudo apt-get install hexchat-indicator # Hexchat Indicator Plugin
 
 # ------------------------------------ #
 # IRC - non-graphical:
