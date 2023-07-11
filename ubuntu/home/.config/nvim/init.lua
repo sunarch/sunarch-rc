db02cc0a361eff919cef9a979eb61db56402ee38
András Németh <sunarch@protonmail.com>
2023-07-11 03:10:56 +0200

    Add initial copied NeoVIM config

---
+++ ubuntu/home/.config/nvim/init.lua
@@ -0,0 +1,19 @@
+-- load options
+require('options')        
+
+-- load keymappings
+require('keymaps')
+
+-- load Packervim
+require('plugins')
+
+-- set colorscheme
+require('colorscheme')
+
+-- set LSP
+require('lsp')
+
+--------------------------------------------------------------------------------
+-- base taken from:
+-- https://martinlwx.github.io/en/config-neovim-from-scratch/
+-- https://github.com/MartinLwx/dotfiles
---
+++ ubuntu/home/.config/nvim/lua/plugins.lua
@@ -0,0 +1,82 @@
+-- Install Packer automatically if it's not installed(Bootstraping)
+-- Hint: string concatenation is done by `..`
+local ensure_packer = function()
+    local fn = vim.fn
+    local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+        vim.cmd [[packadd packer.nvim]]
+        return true
+    end
+    return false
+end
+local packer_bootstrap = ensure_packer()
+
+
+-- Reload configurations if we modify plugins.lua
+-- Hint
+--     <afile> - replaced with the filename of the buffer being manipulated
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]])
+
+
+-- Install plugins here - `use ...`
+-- Packer.nvim hints
+--     after = string or list,           -- Specifies plugins to load before this plugin. See "sequencing" below
+--     config = string or function,      -- Specifies code to run after this plugin is loaded
+--     requires = string or list,        -- Specifies plugin dependencies. See "dependencies". 
+--     ft = string or list,              -- Specifies filetypes which load this plugin.
+--     run = string, function, or table, -- Specify operations to be run after successful installs/updates of a plugin
+return require('packer').startup(function(use)
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
+
+    -- LSP manager
+    use { 'williamboman/mason.nvim' }
+    use { 'williamboman/mason-lspconfig.nvim' }
+    use { 'neovim/nvim-lspconfig' }
+
+    -- Auto-completion engine
+    -- Note:
+    --     the default search path for `require` is ~/.config/nvim/lua
+    --     use a `.` as a path seperator
+    --     the suffix `.lua` is not needed
+    use { 'hrsh7th/nvim-cmp', config = [[require('config.nvim-cmp')]] }
+    use { 'hrsh7th/cmp-nvim-lsp', after = 'nvim-cmp' }
+    use { 'hrsh7th/cmp-buffer', after = 'nvim-cmp' } -- buffer auto-completion
+    use { 'hrsh7th/cmp-path', after = 'nvim-cmp' } -- path auto-completion
+    use { 'hrsh7th/cmp-cmdline', after = 'nvim-cmp' } -- cmdline auto-completion
+    
+    -- Code snippet engine
+    use 'L3MON4D3/LuaSnip'
+    use { 'saadparwaiz1/cmp_luasnip', after = { 'nvim-cmp', 'LuaSnip' } }
+    
+    -- Colorscheme
+    use 'tanvirtin/monokai.nvim'
+    
+    -- Git integration
+    use 'tpope/vim-fugitive'
+    
+    -- Smart indentation for Python
+    use { "Vimjas/vim-python-pep8-indent", ft = { "python" } }
+
+    -- File explorer
+    use {
+        'nvim-tree/nvim-tree.lua',
+        requires = {
+            'nvim-tree/nvim-web-devicons', -- optional, for file icons
+        },
+        config = [[require('config.nvim-tree')]]
+    }
+
+    -- Automatically set up your configuration after cloning packer.nvim
+    -- Put this at the end after all plugins
+    if packer_bootstrap then
+        require('packer').sync()
+    end
+end)
+
