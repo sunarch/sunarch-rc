c0f539151b760ee59dcf86fb74a86a21be5b0e17
András Németh <sunarch@protonmail.com>
2023-08-27 22:12:58 +0200

    Update setup packages

--- ubuntu/zz-setup/packages/06-files.sh
+++ ubuntu/zz-setup/packages/06-files.sh
@@ -6,7 +6,9 @@ sudo apt-get update
 # files:
 
 sudo apt-get install filezilla # Full-featured graphical FTP/FTPS/SFTP client
+sudo apt-get install nemo # File manager and graphical shell for Cinnamon
 sudo apt-get install nextcloud-desktop # Nextcloud folder synchronization tool
+sudo apt-get install thunar # File Manager for Xfce
 
 sudo apt-get install nautilus # file manager and graphical shell for GNOME
 sudo apt-get install nautilus-dropbox # Dropbox integration for Nautilus
@@ -26,5 +28,10 @@ sudo apt-get install gnome-sushi # sushi is a quick previewer for nautilus
 
 sudo apt-get install file-roller # archive manager for GNOME
 sudo apt-get install xarchiver # GTK+ frontend for most used compression formats
+
+# ---------------------------------------------------------------------------- #
+# other
+
+sudo apt-get install exiv2 # EXIF/IPTC/XMP metadata manipulation tool
 
 # ---------------------------------------------------------------------------- #
