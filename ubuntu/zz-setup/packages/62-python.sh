f3d28e40a2f40b7b6fbd553f75056c6c38329252
András Németh <sunarch@protonmail.com>
2024-02-04 14:33:16 +0100

    Update packages list

--- ubuntu/zz-setup/packages/62-python.sh
+++ ubuntu/zz-setup/packages/62-python.sh
@@ -3,6 +3,9 @@
 # ---------------------------------------------------------------------------- #
 # Python PIP packages:
 
+# update pip itself
+pip install --upgrade pip
+
 pip install tldr # command line client for tldr
 
 # ---------------------------------------------------------------------------- #
