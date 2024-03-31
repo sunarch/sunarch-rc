a8f61129acecec184113be8237100250ebf3a4da
András Németh <sunarch@protonmail.com>
2024-03-31 23:24:07 +0200

    Adjust GRUB config and theme

---
+++ ubuntu/boot/grub/themes/copy-to-boot.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+echo "Updating in /boot/grub/themes/"
+
+echo "-> Theme: matrix"
+sudo cp -ruv ./matrix/ /boot/grub/themes/
