f763bceb7c8f5602add75344817f691d6b7e2ec6
András Németh <sunarch@protonmail.com>
2024-03-31 22:44:10 +0200

    Remove GRUB Customizer

--- ubuntu/zz-setup/packages/grub-customizer.sh
+++ ubuntu/zz-setup/packages/grub-customizer.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
+# NOT USED ANYMORE
+
 # ---------------------------------------------------------------------------- #
 # Grub Customizer:
 # - Ubuntu repository version available for certain versions
 
 # https://launchpad.net/grub-customizer
 
-sudo add-apt-repository ppa:danielrichter2007/grub-customizer
-sudo apt-get update
+#sudo add-apt-repository ppa:danielrichter2007/grub-customizer
+#sudo apt-get update
 
-sudo apt-get install grub-customizer # Grub Customizer - A graphical Grub2/BURG configuration application
+#sudo apt-get install grub-customizer # Grub Customizer - A graphical Grub2/BURG configuration application
 
 # ------------------------------------ #
 
