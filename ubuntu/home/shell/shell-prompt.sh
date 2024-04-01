084f323db62589805167da4906adbe323b727dfc
András Németh <sunarch@protonmail.com>
2024-04-01 21:26:28 +0200

    Add function with all Bash prompt special strings

--- ubuntu/home/shell/shell-prompt.sh
+++ ubuntu/home/shell/shell-prompt.sh
@@ -10,10 +10,50 @@ shell-source-include "$HOME/shell/ansi-escape.sh"
 
 # ---------------------------------------------------------------------------- #
 
+# https://www.gnu.org/savannah-checkouts/gnu/bash/manual/bash.html#Controlling-the-Prompt
+
+__prompt_construct_special() {
+    local format=$2
+    local s=''
+
+    case "$1" in
+        bell)        s='\a'   ;; # A bell character.
+        'date')      s='\d'   ;; # The date, in "Weekday Month Date" format (e.g., "Tue May 26").
+        date-fmt)    s='\D{' $format '}' ;; # The format is passed to strftime(3) and the result is inserted into the prompt string; an empty format results in a locale-specific time representation. The braces are required.
+        escape)      s='\e'   ;; # An escape character.
+        host)        s='\h'   ;; # The hostname, up to the first ‘.’.
+        host-full)   s='\H'   ;; # The hostname.
+        'jobs')      s='\j'   ;; # The number of jobs currently managed by the shell.
+        terminal)    s='\l'   ;; # The basename of the shell’s terminal device name.
+        lf)          s='\n'   ;; # A newline.
+        cr)          s='\r'   ;; # A carriage return.
+        shell)       s='\s'   ;; # The name of the shell, the basename of $0 (the portion following the final slash).
+        time-sec-24) s='\t'   ;; # The time, in 24-hour HH:MM:SS format.
+        time-sec-12) s='\T'   ;; # The time, in 12-hour HH:MM:SS format.
+        time-12)     s='\@'   ;; # The time, in 12-hour am/pm format.
+        time-24)     s='\A'   ;; # The time, in 24-hour HH:MM format.
+        username)    s='\u'   ;; # The username of the current user.
+        bash-v)      s='\v'   ;; # The version of Bash (e.g., 2.00)
+        bash-v-full) s='\V'   ;; # The release of Bash, version + patchlevel (e.g., 2.00.0) 
+        'pwd')       s='\w'   ;; # The value of the PWD shell variable ($PWD), with $HOME abbreviated with a tilde (uses the $PROMPT_DIRTRIM variable).
+        pwd-short)   s='\W'   ;; # The basename of $PWD, with $HOME abbreviated with a tilde.
+        history-no)  s='\!'   ;; # The history number of this command.
+        command-no)  s='\#'   ;; # The command number of this command.
+        prompt)      s='\$'   ;; # If the effective uid is 0, #, otherwise $.
+        ascii-oct)   s='\nnn' ;; # The character whose ASCII code is the octal value nnn.
+        backslash)   s='\\'   ;; # A backslash.
+        non-print-begin) s='\['   ;; # Begin a sequence of non-printing characters. This could be used to embed a terminal control sequence into the prompt.
+        non-print-end)   s='\]'   ;; # End a sequence of non-printing characters.
+                      *) s=''
+    esac
+    
+    echo $s
+}
+
 __prompt_construct_nonprinting() {
-    local begin='\['
+    local begin="$(__prompt_construct_special 'non-print-begin')"
     local value="${1}"
-    local end='\]'
+    local end="$(__prompt_construct_special 'non-print-end')"
 
     echo -nE "${begin}${value}${end}"
 }
@@ -34,15 +74,17 @@ __prompt_construct() {
     fi
 
     local prefix_debian_chroot='${debian_chroot:+($debian_chroot)}'
-    local item_user_at_host='\u@\h'
-    local item_pwd='\w'
-    local item_end='\$ '
+    local item_user="$(__prompt_construct_special 'username')"
+    local item_host="$(__prompt_construct_special 'host')"
+    local item_user_at_host="${item_user}@${item_host}"
+    local item_pwd="$(__prompt_construct_special 'pwd')"
+    local item_end="$(__prompt_construct_special 'prompt') "
 
-    local escape_reset="$(ansi_escape_sequence "reset")"
+    local escape_reset="$(ansi_escape_sequence 'reset')"
     local fmt_reset="$(__prompt_construct_nonprinting "${escape_reset}")"
-    local escape_bold_green="$(ansi_escape_sequence "bold" "green")"
+    local escape_bold_green="$(ansi_escape_sequence 'bold' 'green')"
     local fmt_bold_green=$(__prompt_construct_nonprinting "${escape_bold_green}")
-    local escape_bold_blue="$(ansi_escape_sequence "bold" "blue")"
+    local escape_bold_blue="$(ansi_escape_sequence 'bold' 'blue')"
     local fmt_bold_blue=$(__prompt_construct_nonprinting "${escape_bold_blue}")
 
     local prompt=''
@@ -71,6 +113,7 @@ esac
 # ---------------------------------------------------------------------------- #
 # remove unnecessary globals from environment
 
+unset __prompt_construct_special
 unset __prompt_construct_nonprinting
 unset debian_chroot
 
