bbf3b41b758905e946ce48ee584fbbebcb684e5d
András Németh <sunarch@protonmail.com>
2024-04-03 02:39:49 +0200

    Add date to backup names

--- libsystem/settings/dconf-backup.sh
+++ libsystem/settings/dconf-backup.sh
@@ -4,7 +4,9 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-dconf dump / > dconf.preferences
+CURRENT_DATE=$(date +"%Y-%m-%d")
+
+dconf dump / > dconf-backup-${CURRENT_DATE}.preferences
 
 # ---------------------------------------------------------------------------- #
 # to load back:
--- libsystem/settings/gsettings-backup.sh
+++ libsystem/settings/gsettings-backup.sh
@@ -4,4 +4,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-gsettings list-recursively > gsettings.txt
+CURRENT_DATE=$(date +"%Y-%m-%d")
+
+gsettings list-recursively > gsettings-backup-${CURRENT_DATE}.txt
