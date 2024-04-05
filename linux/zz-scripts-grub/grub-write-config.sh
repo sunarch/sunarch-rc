a045a2c13f4fe427daab5308c067834398476e92
András Németh <sunarch@protonmail.com>
2024-04-05 04:09:26 +0200

    Adjust GRUB script

--- linux/zz-scripts-grub/grub-write-config.sh
+++ linux/zz-scripts-grub/grub-write-config.sh
@@ -4,6 +4,8 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-sudo grub-mkconfig -o /boot/grub/grub.cfg
+# Create GRUB configuration file
 
-# same as `update-grub`
+# equivalent commands:
+# sudo grub-mkconfig -o /boot/grub/grub.cfg
+sudo update-grub
