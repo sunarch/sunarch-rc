51ae0d4f41a7a0b9d792fb4ab76b6389fd452f2a
András Németh <sunarch@protonmail.com>
2024-04-05 04:07:48 +0200

    Update license notices and hashbangs

--- libgame/mc-towny/download-tiles.sh
+++ libgame/mc-towny/download-tiles.sh
@@ -1,9 +1,8 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
 
 rm ./tiles-download/*.png
 echo "removed images from tiles-download/"
--- linux/zz-scripts/check-session-type.sh
+++ linux/zz-scripts/check-session-type.sh
@@ -1,9 +1,9 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-
 # check session type
+
 echo "XDG_SESSION_TYPE == '$XDG_SESSION_TYPE'"
