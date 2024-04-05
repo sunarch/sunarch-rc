740aff606fb1cf91c9ba262c8b77071d68cdb5d5
András Németh <sunarch@protonmail.com>
2024-04-05 04:18:19 +0200

    Move unused setup info file to subfolder

--- ubuntu/zz-setup/packages/unused/grub-customizer.sh
+++ ubuntu/zz-setup/packages/unused/grub-customizer.sh
@@ -4,21 +4,25 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-# NOT USED ANYMORE
-
 # ---------------------------------------------------------------------------- #
 # Grub Customizer:
 # - Ubuntu repository version available for certain versions
 
 # https://launchpad.net/grub-customizer
 
-#sudo add-apt-repository ppa:danielrichter2007/grub-customizer
-#sudo apt-get update
+sudo add-apt-repository ppa:danielrichter2007/grub-customizer
+sudo apt-get update
 
-#sudo apt-get install grub-customizer # Grub Customizer - A graphical Grub2/BURG configuration application
+sudo apt-get install grub-customizer # Grub Customizer - A graphical Grub2/BURG configuration application
 
 # ------------------------------------ #
 
 # for 'lunar' it currently needs to be changed back to 'kinetic'
 
 # ---------------------------------------------------------------------------- #
+
+# not used anymore because it got misaligned with new GRUB version in
+#     Ubuntu 23.10 and because it complicates config files
+#
+# see also the following blog post for criticism:
+# https://easylinuxtipsproject.blogspot.com/p/grub-customizer.html
