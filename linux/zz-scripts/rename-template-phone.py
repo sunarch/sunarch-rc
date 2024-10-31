28ee44ac1b7227dae5200704c4099207aac26b73
András Németh <sunarch@protonmail.com>
2024-10-31 08:19:03 +0100

    Add batch renaming script

---
+++ linux/zz-scripts/rename-template-phone.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import os
+import os.path
+import string
+
+DIGIT_SET: set[str] = set(string.digits)
+
+def create_mapping(filename: str) -> tuple[str, str]:
+    name, ext = os.path.splitext(filename)
+
+    components: tuple[str] = name.split('_')
+    iteration: str = ""
+    if len(components) == 1:
+        print(f'Invalid format: no underscore - "{filename}"')
+        exit()
+    elif len(components) == 2:
+        pass
+    elif len(components) == 3:
+        iteration = '-{}'.format(components[2])
+    else:
+        print(f'Invalid format: "{filename}"')
+        exit()
+
+    date = components[0]
+    date_set: set[str] = set(date)
+    time = components[1]
+    time_set: set[str] = set(time)
+    if len(date) != 8:
+        print(f'Invalid date length: "{filename}"')
+        exit()
+    if not date_set.issubset(DIGIT_SET):
+        diff: set[str] = date_set - DIGIT_SET
+        print(f'Invalid date character in "{filename}": "{diff}"')
+        exit()
+    if len(time) != 6:
+        print(f'Invalid time length: "{filename}"')
+        exit()
+    if not time_set.issubset(DIGIT_SET):
+        diff: set[str] = time_set - DIGIT_SET
+        print(f'Invalid time character in "{filename}": "{diff}"')
+        exit()
+        
+    year: str = date[0:4]
+    month: str = date[4:6]
+    day: str = date[6:8]
+    
+    hours_and_mins: str = time[0:4]
+    secs: str = time[4:6]
+    
+    new_filename: str = f'{year}-{month}-{day}-{hours_and_mins}-{secs}{iteration}{ext}'
+    
+    #print(f'Mapping for "{filename}" : "{new_filename}"')
+    
+    return (filename, new_filename)
+
+def main():
+    filenames: list[str] = list(sorted(os.listdir()))
+
+    mapping: list[tuple[str, str]] = list(map(create_mapping, filenames))
+
+    #print()
+
+    for old_filename, new_filename in mapping:
+        print(f'Renaming: "{old_filename}" -> "{new_filename}"')
+        os.rename(old_filename, new_filename)
+        
+if __name__ == '__main__':
+    main()
